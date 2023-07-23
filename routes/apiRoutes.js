d1023648ad3f957e03f065f7ac56883338310df7
charbelk07 <125899348+charbelk07@users.noreply.github.com>
2023-07-23 13:32:28 -0700

    defined api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,8 @@
+const api = require ('express').Router();
+
+
+
+
+
+
+module.exports = api;
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const htmlRoutes = require("./htmlRoutes");
+const apiRoutes = require("./apiRoutes");
+const router = require("express").Router();
+
+router.use('/api', apiRoutes)
+router.use('/', htmlRoutes)
+
+
+
+module.exports = router;
