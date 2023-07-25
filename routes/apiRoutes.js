ae3d9e2a12d034a805c3fc6e9f69805e6deb10aa
charbelk07 <125899348+charbelk07@users.noreply.github.com>
2023-07-25 07:37:05 -0700

    api routes updated2

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,37 @@
 const api = require("express").Router();
-const {readFromFile, ReadAmdAppend, readAndAppend} = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const {
+  readFromFile,
+  ReadAmdAppend,
+  readAndAppend,
+} = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
 
 //get method to retrieve data in db.json
 api.get("/notes", (req, res) => {
-    readFromFile('./db/db.json').then((data)=>res.json(JSON.parse(data)));
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-api.post("/notes" , (req, res)=>{
-    console.info(`${req.method} request received to add notes`)
-    console.log(req.body);
-    
-    const {title, text} = req.body
-    if (req.body){
-        const  newNote = {
-            title,
-            text,
-        };
-        readAndAppend(newNote, './db/db.json');
-        res.json(`New Note Added Successfully`);
-    }
-    else {
-        res.error('Error in Adding Note');
-    }
-       
+api.post("/notes", (req, res) => {
+  console.info(`${req.method} request received to add notes`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      user_id: uuid(),
+    };
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`New Note Added Successfully`);
+  } else {
+    res.error("Error in Adding Note");
+  }
 });
 
+// api.get('/notes/:user_id',(req,res)=>{
+//     readFromFile('./db/db.json').then((data)=>res.json(JSON.parse(data)));
+//     console.info(`${req.method} request received to get a single user_id`)
+
+// })
 module.exports = api;
